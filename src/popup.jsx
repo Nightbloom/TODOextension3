fbe36317100b720bd2947cc8e6b1c154da8db808
Siddharth Sharma <siddharthsharma653@gmail.com>
2022-01-08 21:52:21 +0530

    2nd commit

--- src/popup.jsx
+++ src/popup.jsx
@@ -2,18 +2,42 @@ import react from "react";
 import { render } from "react-dom";
 
 function Popup(){
+    const items = [{"item": "This is it", "status": 0},
+    {"item": "This is it", "status": 0}]
+
+    const itemStr = JSON.stringify(items)
+    console.log(items);
+    console.log(itemStr);
+
     return (
         <>
         <h1>hello</h1>
         <h2>thisis me</h2>
-        <ul>
-            <li><a href="">thisi is me</a></li>
-            <li><a href="">thisi is me</a></li>
-            <li><a href="">thisi is me</a></li>
+        <button className="create-todo">New Item</button>
+        <div className="new-item">
+            <input type="text" />
+            <button>Save</button>
+        </div>
+        <ul className="todo-items">
+            <li className="done item" onClick={head}>This is me <div><button>A</button><button>D</button></div></li>
+            <li >This is me<div><button>A</button><button>D</button></div></li>
+            <li >This is me<div><button>A</button><button>D</button></div></li>
         </ul>
         </>
         
     );
+}
+
+function fetchItem(){
+    let items = localStorage.getItem('todo-items')
+}
+
+function saveItem(){
+
+}
+
+function head(){
+    console.log("yes it is working");
 }
 
 render(<Popup/>,document.getElementById("react-target"));
